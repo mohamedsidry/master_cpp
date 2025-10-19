83698e69486220295e828d554d006d8f37775a40
mohamedsidry <mohamed.sidry1999@gmail.com>
2025-10-19 16:49:52 +0100

    ex00 done!
    pointer and scope !

---
+++ cpp_module_01/ex00/newZombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+
+Zombie* Zombie::newZombie(std::string name)
+{
+    Zombie* zombie;
+    zombie = NULL;
+    try
+    {
+        zombie = new Zombie(name);
+    }
+    catch (const std::bad_alloc &e)
+    {
+        std::cerr << "Allocation failed: " << e.what() << std::endl;
+    }
+    return (zombie);
+}
