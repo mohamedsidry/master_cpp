3bd1ece355180310d11194fa1802aa727d50f2fb
Mohamed Sidry <msidry@c2r8p6.1337.ma>
2026-04-28 11:36:22 +0100

    test(main): add tests .

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -10,51 +10,83 @@
 
 
 
+void printint(int i)
+{
+    std::cout << i << " ";
+}
+
+
 int main(void)
 {
-    MutantStack<int> mstack;
-
-    mstack.push(42);
-    mstack.push(512);
-
-    std::cout << mstack.top() << std::endl;
-
-    mstack.pop();
-
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    std::cout << mstack.size() << std::endl;
-
-
-
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    it++;
-    it--;
-    while (it != ite)
     {
-        std::cout << "value is : " << *it << std::endl;
+        std::clog << "\033[1;33m" << "Test 0 : " << "\033[0m" << "Default Stack ." << "\033[0m" << std::endl;
+        MutantStack<int> mstack;
+        mstack.push(1);
+        mstack.push(2);
+        mstack.push(4);
+        mstack.push(8);
+        mstack.push(16);
+        mstack.push(32);
+        mstack.push(64);
+        mstack.push(128);
+        std::for_each(mstack.begin(), mstack.end(), printint);
+        std::cout << std::endl;
+        std::cout << "Stack size : " << mstack.size() << std::endl;
+    }
+
+    {
+        std::clog << "\033[1;33m" << "Test 1 : " << "\033[0m" << "Copy Stack ." << "\033[0m" << std::endl;
+        MutantStack<int> stack1;
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(4);
+        MutantStack<int> stack2(stack1);
+        std::for_each(stack2.begin(), stack2.end(), printint);
+        std::cout << std::endl;
+        std::cout << "Stack size : " << stack2.size() << std::endl;
+    }
+
+    {
+        std::clog << "\033[1;33m" << "Test 2 : " << "\033[0m" << "Stack iterator ." << "\033[0m" << std::endl;
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        mstack.pop();
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
         it++;
+        it--;
+        while (it != ite)
+        {
+            std::cout << *it << " ";
+            it++;
+        }
+        std::cout << std::endl;
+        std::cout << "Stack size : " << mstack.size() << std::endl;
     }
-    std::stack<int> s(mstack);
-    while (s.size())
-    {
-        std::cout << " --- value is : " << s.top() << std::endl;
-        s.pop();
-    }
-    MutantStack<int> s2 = mstack;
 
-    MutantStack<int>::iterator it2 = mstack.begin();
-    MutantStack<int>::iterator ite2 = mstack.end();
-    it2++;
-    it2--;
-    while (it2 != ite2)
     {
-        std::cout << "value is : " << *it2 << std::endl;
-        it2++;
+        std::clog << "\033[1;33m" << "Test 3 : " << "\033[0m" << "Stack const iterator ." << "\033[0m" << std::endl;
+        MutantStack<int> stack1;
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(4);
+        const MutantStack<int> stack2(stack1);
+        MutantStack<int>::const_iterator it = stack2.cbegin();
+        MutantStack<int>::const_iterator ite = stack2.cend();
+        it++;
+        it--;
+        while (it != ite)
+        {
+            std::cout << *it << " ";
+            it++;
+        }
+        std::cout << std::endl;
+        std::cout << "Stack size : " << stack2.size() << std::endl;
     }
     return 0;
 }
